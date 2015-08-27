3cf855827bd9e4be29ab9b2211f79556c6150534
Zach Obermiller <oberm064@umn.edu>
2015-08-27 16:24:06 -0500

    Renamed main.cpp to win32_pong.cpp and added a corresponding header file to hold onto structs and what not

---
+++ src/win32_pong.h
@@ -0,0 +1,70 @@
+#ifndef WIN32_PONG_H
+#define WIN32_PONG_H
+
+typedef int8_t s8;
+typedef int16_t s16;
+typedef int32_t s32;
+typedef int64_t s64;
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+#define kilobytes(value) ((value) * 1024LL)
+#define megabytes(value) (kilobytes(value) * 1024LL)
+#define gigabytes(value) (megabytes(value) * 1024LL)
+
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
+
+enum wall {
+	WALL_NONE,
+	WALL_LEFT,
+	WALL_UP,
+	WALL_RIGHT,
+	WALL_DOWN,
+};
+
+struct offscreen_buffer {
+	void* memory;
+	s32 height;
+	s32 width;
+	s32 bitsPerPixel;
+	s32 pitch;
+};
+
+struct game_memory {
+	void* storage;
+	u32 storageSize;
+};
+
+struct player {
+	v2 paddlePos;
+	// Size is (width, height)
+	v2 size;
+	u32 score;
+
+	v2 vertices[4];
+};
+
+struct ball {
+	v2 ballPos;
+	// Size is (width, height)
+	v2 size;
+	v2 velocity;
+
+	v2 vertices[4];
+};
+
+struct game_state {
+	player players[2];
+	ball theBall;
+
+	u32 arenaWidth, arenaHeight;
+
+	u32 staticVerticesCount;
+	v2 staticVertices[SCREEN_HEIGHT];
+};
+
+#endif
