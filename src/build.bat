fa56625977d3fb31fdae311c11c54fdde8852ed1
Zach Obermiller <zobermiller@gmail.com>
2016-10-14 23:37:41 -0500

    Also added a build.bat. Moved away from inside Visual Studio. Using 'cl' for compiling. Using Visual Studio for debugging only (May add a solution for the .exe later)

---
+++ src/build.bat
@@ -0,0 +1,12 @@
+@echo off
+
+IF NOT DEFINED clset (call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x64)
+SET clset=64
+
+IF NOT EXIST ..\build mkdir ..\build
+pushd ..\build
+
+set CompilerFlags=-Od -MTd -nologo -fp:fast -fp:except- -Gm- -GR- -EHa- -Zo -Oi -Z7 -WX -W4 -wd4127 -wd4201 -wd4100 -wd4189 -wd4505
+set LinkerFlags= -incremental:no -opt:ref gdi32.lib user32.lib winmm.lib opengl32.lib
+
+cl %CompilerFlags% ../src/win32_pong.cpp /link %LinkerFlags%
